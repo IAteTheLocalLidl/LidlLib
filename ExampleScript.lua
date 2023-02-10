3decbede66f43023804073d86c8287dba29b8c7d
IAteTheLocalLidl <124361449+IAteTheLocalLidl@users.noreply.github.com>
2023-02-10 14:30:43 +0200

    testing

--- ExampleScript.lua
+++ ExampleScript.lua
@@ -9,11 +9,22 @@ local lib = _G.GetLidlLibrary()
 --Create a blank UI to use
 local ui = lib:CreateGui()
 
+--Please wait a bit until you can proceed forward
+repeat task.wait(0.1)
+until ui
+
 --Change the title of the UI
 --For its arguments, put in the ui and then the name
-lib:ChangeNameTitle(ui, "LidlLib Test")
+lib:ChangeNameTitle("LidlLib Example")
+
+--Add a folder (basically a group of buttons)
+--For its arguments, put in the name of the category of the folder (really anything)
+lib:NewButtonFolder("Local Cheats")
+
+--Lets make another one for no reason at all
+lib:NewButtonFolder("Extrasensory Perception")
 
 --ONLY use after you have setted up everything in this ui (all folders, all buttons, etc)
---AddToGui is best used on synapse as that has a function to protect the ui
+--Ready is best used on synapse as that has a function to protect the ui
 --For its arguments, put in the ui
-lib:AddToGui(ui)
+lib:Ready(ui)
